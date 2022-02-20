5cd51589add44e90d01e489f39a018357b5c0c7d
Vladyslav Stulii <Killerische@gmail.com>
2022-02-20 21:36:39 +0200

    initial sort_arr.h

--- Git/main.cpp
+++ Git/main.cpp
@@ -1,11 +1,12 @@
 #define _USE_MATH_DEFINES
-#include <iostream>
+
 #include <math.h> 
 #include <windows.h>
 #include <conio.h>
 #include <cstdlib>
 #include <string>
 #include <iomanip>
+#include "sort_arr.h"
 using namespace std;
 HANDLE hConsole;
 
