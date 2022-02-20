d95230bfdcd4feed6aca1371e44c35c45ed1a45f
Vladyslav Stulii <Killerische@gmail.com>
2022-02-20 21:21:41 +0200

    Initial commit

---
+++ Git/main.cpp
@@ -0,0 +1,20 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <math.h> 
+#include <windows.h>
+#include <conio.h>
+#include <cstdlib>
+#include <string>
+#include <iomanip>
+using namespace std;
+HANDLE hConsole;
+
+
+
+
+
+
+int main()
+{
+	return 0;
+}
